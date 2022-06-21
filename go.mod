2f2cc6dc922f6aa8d6950583723ecf187f350089
张宇 <zhangyu.me@bytedance.com>
2022-06-21 15:27:08 +0800

    feat: fot-test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ccc-me/for-golang-test
 
 go 1.16
+
+require github.com/volcengine/vefaas-golang-runtime v0.0.0-20220324111123-998a3428ddcb
--- main.go
+++ main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"net/http"
+
+	"github.com/volcengine/vefaas-golang-runtime/events"
+	"github.com/volcengine/vefaas-golang-runtime/vefaas"
+	"github.com/volcengine/vefaas-golang-runtime/vefaascontext"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
-func ping(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "pong!\n")
-}
-
 func main() {
+	// Start your vefaas function =D.
+	vefaas.Start(handler)
+}
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-	http.HandleFunc("/v1/ping", ping)
-
-	http.ListenAndServe(":8000", nil)
+// Define your handler function.
+func handler(ctx context.Context, r *events.HTTPRequest) (*events.EventResponse, error) {
+	fmt.Printf("received new request: %s %s, request id: %s\n", r.HTTPMethod, r.Path, vefaascontext.RequestIdFromContext(ctx))
+	fmt.Printf("debug request: header:%v, body:%s\n", r.Headers, r.Body)
+	ret := make(map[string]interface{})
+	ret["content"] = "Hello veFaaS from louyuting-v1!"
+	ret["http_method"] = r.HTTPMethod
+	ret["http_path"] = r.Path
+	query := make(map[string]interface{})
+	header := make(map[string]interface{})
+	for k, v := range r.QueryStringParameters {
+		query[k] = v
+	}
+	for k, v := range r.Headers {
+		header[k] = v
+	}
+	ret["http_query"] = query
+	ret["http_header"] = header
+	ret["http_body"] = string(r.Body)
+	retBody, _ := json.Marshal(ret)
+	return &events.EventResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: retBody,
+	}, nil
 }
